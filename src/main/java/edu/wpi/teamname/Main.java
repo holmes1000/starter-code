b7f721a4e7177e08a78cd07ebbe76444869f3d1f
Samara Holmes <46973232+holmes1000@users.noreply.github.com>
2023-03-20 23:44:36 -0400

    added test code

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
   public static void main(String[] args) {
     App.launch(App.class, args);
+    System.out.println("This is a test");
+    System.out.println("Samara");
   }
 
   // shortcut: psvm
